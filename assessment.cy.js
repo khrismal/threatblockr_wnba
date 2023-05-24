53bdb7eff8405fbd26449721a1b0e51a2f1ac04e
christina <khristina.malysheva@gmail.com>
2023-05-24 19:17:49 -0400

    Code comments

--- assessment.cy.js
+++ assessment.cy.js
@@ -1,18 +1,23 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
-
+//cypress test to check the numbers of WNBA teams and print their names
 describe('My assigment', () =>{
-  it('Test WBMA website', () => {
+  it('Test WNBA website', () => {
+
+    //load the WBNA web page
     cy.visit('https://www.wnba.com/standings/')
 
+    //select season 2018 
    cy.get('select[id="Season-dropdown"]').select('2018',{force:true})
 
+    //assert that number of table records is 12
    cy.get('tbody tr').should('have.length', 12)
+
+   //iterate over the table 
    cy.get('tbody > tr  > td:nth-child(1)').each(($elm)=> {
+
+    //declare variable and get text from it
     const t = $elm.text();
+
+    //print team names using cypress log API
     cy.log(t)  
    })
   })
